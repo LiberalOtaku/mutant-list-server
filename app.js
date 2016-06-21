8eebde7ba3147f3d2204bfe3ed7d5df30fdccb1f
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-21 15:06:03 -0400

    Add firebase authentication.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+// Create references for libraries
 var express = require('express');
 var http = require('http');
+var firebase = require('firebase');
 
+// Express server setup
 var app = express();
 var server = http.createServer(app);
+
+// Authenticate with Firebase
+firebase.initializeApp({
+  serviceAccount: "firebase-credentials.json",
+  databaseURL: "https://mutant-hours-f0ec3.firebaseio.com"
+});
 
 server.listen(3030, function() {
   console.log('Listening on http://localhost:3030...');
