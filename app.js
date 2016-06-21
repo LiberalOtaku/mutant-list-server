490c6b387336dc9cc0e14bb6a65c639b8148db59
LiberalOtaku <ledgam3r1279@gmail.com>
2016-06-21 15:44:56 -0400

    Send texts through Twilio.

--- app.js
+++ app.js
@@ -2,15 +2,36 @@
 var express = require('express');
 var http = require('http');
 var firebase = require('firebase');
+var twilio = require('twilio');
+var dotenv = require('dotenv');
 
 // Express server setup
 var app = express();
 var server = http.createServer(app);
+dotenv.load();
 
 // Authenticate with Firebase
 firebase.initializeApp({
   serviceAccount: "firebase-credentials.json",
   databaseURL: "https://mutant-hours-f0ec3.firebaseio.com"
+});
+var rootRef = firebase.database().ref();
+
+// Authenticate with twilio
+var twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+
+// Listen for new texts being added
+var textsRef = rootRef.child('texts');
+textsRef.on('child_added', function(snapshot) {
+  var text = snapshot.val();
+  twilioClient.messages.create({
+    body: text.name + ', I\'m available to see you now. Please come to my office so we can discuss: ' + text.topic + '.',
+    to: text.phone,  // Text this number
+    from: process.env.TWILIO_PHONE // From a valid Twilio number
+  }, function(err, message) {
+    if (err)
+      console.log(message.sid);
+  });
 });
 
 server.listen(3030, function() {
